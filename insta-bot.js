0037e38186201d108d8cb2dd954b68560828ed3f
Arshad Yaseen <arshadpyaseen@gmail.com>
2022-06-19 13:04:15 +0530

    Solved bot supports in specific browsers 👏 now supports all browsers

--- insta-bot.js
+++ insta-bot.js
@@ -1,5 +1,5 @@
-let followbutton = document.querySelectorAll('.PolarisIGButton')[1]
-let unfollowbutton = document.querySelectorAll('.PolarisIGButton')[1]
+let followbutton = document.querySelectorAll('.PolarisIGButton')[1] || document.querySelectorAll('._acan')[0]
+let unfollowbutton = document.querySelectorAll('.PolarisIGButton')[1] || document.querySelectorAll('._acan')[0]
 
 let timeBetweenClick = 2000;
 let timeBetweenFollowUnfollow = 30000;
